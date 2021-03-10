f28bdc78b4c11889c04eacb3e4f86053ecbe432c
OutOfStack <ftavlad@gmail.com>
2021-03-11 00:54:26 +0300

    Sales create, retrieve API

--- internal/app/game-library-api/game/models.go
+++ internal/app/game-library-api/game/models.go
@@ -7,16 +7,16 @@ import (
 
 // Game represents game
 type Game struct {
-	ID          int            `db:"id" json:"id"`
+	ID          int64          `db:"id" json:"id"`
 	Name        string         `db:"name" json:"name"`
 	Developer   string         `db:"developer" json:"developer"`
 	ReleaseDate types.Date     `db:"release_date" json:"releaseDate"`
 	Genre       pq.StringArray `db:"genre" json:"genre"`
 }
 
-// PostModel represents game model we receive from user
+// NewGame represents game data we receive from user
 // TODO: change releaseDate type to time.TIme (requires custom JSON marsaller and unmarhsller)
-type PostModel struct {
+type NewGame struct {
 	Name        string         `json:"name"`
 	Developer   string         `json:"developer"`
 	ReleaseDate string         `json:"releaseDate"`
@@ -25,10 +25,18 @@ type PostModel struct {
 
 // Sale respresents information about game being on sale
 type Sale struct {
-	ID              int        `db:"id" json:"id"`
+	ID              int64      `db:"id" json:"id"`
 	Name            string     `db:"name" json:"name"`
-	GameID          int        `db:"game_id" json:"gameId"`
+	GameID          int64      `db:"game_id" json:"gameId"`
 	BeginDate       types.Date `db:"begin_date" json:"beginDate"`
 	EndDate         types.Date `db:"end_date" json:"endDate"`
 	DiscountPercent uint8      `db:"discount_percent" json:"discountPercent"`
+}
+
+// NewSale represents sale date we receive from user
+type NewSale struct {
+	Name            string `json:"name"`
+	BeginDate       string `json:"beginDate"`
+	EndDate         string `json:"endDate"`
+	DiscountPercent uint8  `json:"discountPercent"`
 }

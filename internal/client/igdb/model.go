2abae14d6c24c4d35cc4768d895d658329c8b425
OutOfStack <ftavlad@gmail.com>
2022-11-28 00:43:49 +0300

    Added get top rated games from igdb method

--- internal/client/igdb/model.go
+++ internal/client/igdb/model.go
@@ -6,3 +6,11 @@ type TokenResp struct {
 	ExpiresIn   int64  `json:"expires_in"`
 	TokenType   string `json:"token_type"`
 }
+
+// TopRatedGamesResp - top rated games response
+type TopRatedGamesResp struct {
+	ID          uint32  `json:"id"`
+	Name        string  `json:"name"`
+	Rating      float32 `json:"rating"`
+	RatingCount uint32  `json:"rating_count"`
+}
--- internal/client/igdb/token.go
+++ internal/client/igdb/token.go
@@ -1,14 +1,8 @@
 package igdb
 
 import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"sync"
 	"time"
-
-	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
 )
 
 type token struct {
@@ -31,37 +25,4 @@ func (t *token) set(token string, expiresIn int64) {
 	defer t.mu.Unlock()
 	t.token = token
 	t.expiresAt = time.Now().Add(time.Duration(expiresIn) * time.Second)
-}
-
-// accessToken returns access token
-func (c *Client) accessToken(ctx context.Context) (string, error) {
-	token := c.token.get()
-	if token != "" {
-		return token, nil
-	}
-
-	req, err := http.NewRequestWithContext(ctx, "POST", c.conf.TokenURL, nil)
-	if err != nil {
-		return "", fmt.Errorf("error creating verify request")
-	}
-
-	q := req.URL.Query()
-	q.Add("grant_type", "client_credentials")
-	q.Add("client_id", c.conf.ClientID)
-	q.Add("client_secret", c.conf.ClientSecret)
-	req.URL.RawQuery = q.Encode()
-
-	resp, err := otelhttp.DefaultClient.Do(req)
-	if err != nil {
-		c.log.Printf("error calling token api at %s: %v\n", c.conf.TokenURL, err)
-		return "", fmt.Errorf("token api unavailable: %v", err)
-	}
-	defer resp.Body.Close()
-
-	var respBody TokenResp
-	json.NewDecoder(resp.Body).Decode(&respBody)
-
-	c.token.set(respBody.AccessToken, respBody.ExpiresIn)
-
-	return respBody.AccessToken, nil
 }
